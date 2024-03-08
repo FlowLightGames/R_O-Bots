2e62512c62dcf127af68197bbd1324a5d5c2f9ba
FlowLightGames <75386913+FlowLightGames@users.noreply.github.com>
2024-03-08 21:12:56 +0100

    again

--- menus/online_lobby_list.gd
+++ menus/online_lobby_list.gd
@@ -42,7 +42,7 @@ func _on_friend_search_pressed()->void:
 	for key:int in friend_lobbys.keys():
 		var lobby_info:Dictionary=friend_lobbys[key]
 		var lobby_name:String=lobby_info["name"]
-		var friend_ids:Array[int]=lobby_info["friend"]
+		var friend_id:int=lobby_info["friend"]
 		var max_members:int= Steam.getLobbyMemberLimit(key)
 		var current_members:int=Steam.getNumLobbyMembers(key)
 		var host_id:int=Steam.getLobbyOwner(key)
