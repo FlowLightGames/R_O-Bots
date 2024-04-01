107fecc489e7ae95c127d5b98728ff38369bdcc6
FlowLightGames <75386913+FlowLightGames@users.noreply.github.com>
2024-04-02 01:43:57 +0200

    MADE OPTIONS AND CURSOR AND CRT

--- menus/character_creator/custom_face_creator.gd
+++ menus/character_creator/custom_face_creator.gd
@@ -15,15 +15,9 @@ var max_number_face_color:int=8
 @export
 var body_color_txt:Label
 @export
-var body_color_txt_shdw:Label
-@export
 var body_txt:Label
 @export
-var body_txt_shdw:Label
-@export
 var face_color_txt:Label
-@export
-var face_color_txt_shdw:Label
 
 
 var current_body_color:int=0
@@ -52,7 +46,6 @@ func _on_dec_body_base_pressed()->void:
 	example_character_0.set_new_body(current_body_base)
 	example_character_1.set_new_body(current_body_base)
 	body_txt.text=str(current_body_base)
-	body_txt_shdw.text=str(current_body_base)
 
 
 func _on_inc_body_base_pressed()->void:
@@ -60,7 +53,6 @@ func _on_inc_body_base_pressed()->void:
 	example_character_0.set_new_body(current_body_base)
 	example_character_1.set_new_body(current_body_base)
 	body_txt.text=str(current_body_base)
-	body_txt_shdw.text=str(current_body_base)
 
 
 func _on_dec_body_color_pressed()->void:
@@ -68,21 +60,18 @@ func _on_dec_body_color_pressed()->void:
 	example_character_0.set_new_body_color(current_body_color)
 	example_character_1.set_new_body_color(current_body_color)
 	body_color_txt.text=str(current_body_color)
-	body_color_txt_shdw.text=str(current_body_color)
 
 func _on_inc_body_color_pressed()->void:
 	current_body_color=posmod(current_body_color+1,max_number_body_color)
 	example_character_0.set_new_body_color(current_body_color)
 	example_character_1.set_new_body_color(current_body_color)
 	body_color_txt.text=str(current_body_color)
-	body_color_txt_shdw.text=str(current_body_color)
 
 func _on_dec_face_color_pressed()->void:
 	current_face_color=posmod(current_face_color-1,max_number_face_color)
 	example_character_0.set_new_face_color(current_face_color)
 	example_character_1.set_new_face_color(current_face_color)
 	face_color_txt.text=str(current_face_color)
-	face_color_txt_shdw.text=str(current_face_color)
 
 
 func _on_inc_face_color_pressed()->void:
@@ -90,5 +79,4 @@ func _on_inc_face_color_pressed()->void:
 	example_character_0.set_new_face_color(current_face_color)
 	example_character_1.set_new_face_color(current_face_color)
 	face_color_txt.text=str(current_face_color)
-	face_color_txt_shdw.text=str(current_face_color)
 
---
+++ menus/options/options.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+enum State{
+	DEFAULT,AUDIO,VIDEO,CUSTOM
+}
+
+
+func _on_audio_pressed()->void:
+	pass # Replace with function body.
+
+
+func _on_video_pressed()->void:
+	pass # Replace with function body.
+
+
+func _on_customize_pressed()->void:
+	pass # Replace with function body.
