c38e40c779140792e79a8ad0e2b19ffc27ee04d7
FlowLightGames <75386913+FlowLightGames@users.noreply.github.com>
2024-05-15 01:14:59 +0200

    adjusted fireball + assets

--- player_character/fire/fire_ball.gd
+++ player_character/fire/fire_ball.gd
@@ -4,24 +4,39 @@ class_name FireBall
 var direction:Vector2=Vector2.ZERO
 var ignored_bodies:Array[Node2D]=[]
 var speed:int=60
+var disabled:bool=false
 @export var sprite:Sprite2D
+@export var death_poof:GPUParticles2D
+@export var trail:GPUParticles2D
 
 func _ready()->void:
 	sprite.rotation=direction.angle()
 
 func _physics_process(delta:float)->void:
-	position+=(direction*speed*delta)
+	if !disabled:
+		position+=(direction*speed*delta)
+
+func init_free()->void:
+	disabled=true
+	trail.emitting=false
+	death_poof.emitting=true
+	sprite.self_modulate=Color(0.0,0.0,0.0,0.0)
 
 
 func _on_body_entered(body:Node2D)->void:
-	if !(body in ignored_bodies):
-		if body is PlayerCharacter:
-			body.damage()
-			queue_free()
-		elif body is EnemyBase:
-			body.damage()
-			queue_free()
-		elif body is BombBase:
-			(body as BombBase).explode()
-		else:
-			queue_free()
+	if !disabled:
+		if !(body in ignored_bodies):
+			if body is PlayerCharacter:
+				body.damage()
+				init_free()
+			elif body is EnemyBase:
+				body.damage()
+				init_free()
+			elif body is BombBase:
+				(body as BombBase).explode()
+			else:
+				init_free()
+
+
+func _on_die_poof_finished()->void:
+	queue_free()
