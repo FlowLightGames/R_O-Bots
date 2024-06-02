488a3cf826c9e7fdbd87ec59d4c4097795090fc9
FlowLightGames <75386913+FlowLightGames@users.noreply.github.com>
2024-06-02 22:06:01 +0200

    testo

--- menus/stage_select/stage_select.gd
+++ menus/stage_select/stage_select.gd
@@ -18,40 +18,42 @@ var current_size:SIZE=SIZE.M
 var current_stage:int=0
 
 func _ready()->void:
-	if !MultiplayerStatus.Current_Status==MultiplayerStatus.STATE.ONLINE_LOBBY:
-		for n:Node in multiplayer_exlusive_nodes:
-			n.free()
+	
 	for n:StageSelectMetaData in stage_select_meta_datas:
 		n.stage_pickup_map.sort_map()
 	max_stages=stage_select_meta_datas.size()
 	current_size=SIZE.M
 	current_stage=0
 	apply_new_selection()
+	
+	if MultiplayerStatus.Current_Status==MultiplayerStatus.STATE.ONLINE_LOBBY:
+		PackageDeconstructor.stage_selected.connect(_on_stage_start_recieved)
+	else:
+		for n:Node in multiplayer_exlusive_nodes:
+			n.free()
+	
+
 
 
 func apply_new_selection()->void:
 	size_label.text=SIZE.keys()[current_size]
 	stage_label.text=str(current_stage)
 	stage_preview.set_tileset_preview(stage_select_meta_datas[current_stage])
-	
 
 func _on_stage_dec_pressed()->void:
 	if SteamLobby.is_host:
 		current_stage=posmod(current_stage-1,max_stages)
 		apply_new_selection()
 
-
 func _on_stage_inc_pressed()->void:
 	if SteamLobby.is_host:
 		current_stage=posmod(current_stage+1,max_stages)
 		apply_new_selection()
 
-
 func _on_size_dec_pressed()->void:
 	if SteamLobby.is_host:
 		current_size=posmod(current_size-1,max_sizes)
 		apply_new_selection()
-
 
 func _on_size_inc_pressed()->void:
 	if SteamLobby.is_host:
@@ -86,7 +88,10 @@ func _on_go_pressed()->void:
 			var selected_scene:PackedScene=get_map_scene(current_stage,current_size)
 			if selected_scene:
 				for n:int in range(1,MultiplayerStatus.Current_Number_Of_Players):
-					var msg:PackedByteArray=PackageConstructor.stage_start_up_master(SteamLobby.random_seed,PlayerConfigs.Player_Configs[n].message_delay_tcp,1,1)
+					var msg:PackedByteArray=PackageConstructor.stage_start_up_master(SteamLobby.random_seed,
+					PlayerConfigs.Player_Configs[n].message_delay_tcp,
+					current_stage,
+					int(current_size))
 					SteamLobby.send_p2p_packet(PlayerConfigs.Player_Configs[n].steam_id,Steam.P2P_SEND_RELIABLE,msg)
 				var stage:PackedScene=load("res://maps/instances/default_M.tscn") as PackedScene
 				MultiplayerStatus.Current_Status=MultiplayerStatus.STATE.ONLINE_MULTIPLAYER
